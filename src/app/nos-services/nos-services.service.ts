d093442ebd1e9d767a2052580b2fb3b119636a35
katy <akane@ssi.sn>
2021-03-11 16:46:37 +0000

    front-end module service

--- src/app/nos-services/nos-services.service.ts
+++ src/app/nos-services/nos-services.service.ts
@@ -13,36 +13,9 @@ export class NosServicesService {
   }
 
   // tslint:disable-next-line:typedef
-  public listNosServices() {
-
-    return this.Http.get<any>(this.urlWs + '/nosServices');
-
-  }
-
-  // tslint:disable-next-line:typedef
-  public deleteNosServices(idNosServices) {
-
-    return this.Http.delete<any>(this.urlWs + '/nosServices/' + idNosServices);
-
-  }
-  // tslint:disable-next-line:typedef
   public findNosServices(idNosServices) {
 
     return this.Http.get<any>(this.urlWs + '/nosServices/' + idNosServices);
-
-  }
-  // tslint:disable-next-line:typedef
-  public addNosServices(nosServices, idtypeservice) {
-
-    return this.Http.post<any>(this.urlWs + '/nosServices/typeservice/' + idtypeservice, nosServices);
-
-  }
-
-  // tslint:disable-next-line:typedef
-  public editNosServices(idNosServices, nosServices, idtypeservice) {
-
-    // @ts-ignore
-    return this.Http.put<any>(this.urlWs + '/nosServices/' +  idNosServices + '/typeservice/' + idtypeservice, nosServices);
 
   }
 }
